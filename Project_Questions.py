c3a0a65514faeedbf381568e7a22f2905868b682
DESKTOP-UJIMONJ\zofia <zhs17@hampshire.edu>
2018-05-03 10:31:00 -0400

    Rebuilding foundation and starting questions.

---
+++ Project_Questions.py
@@ -0,0 +1,40 @@
+def Anagrams():
+    anagrams = {
+        "1. What does ASH stand for?" : "adele simmons hall",
+        "2. What does FPL stand for?" : "...",
+        "3. What does EDH stand for?" : "emily dickinson hall",
+        "4. What does HJL stand for?" : "harold johnson library",
+        "5. What does APL stand for?" : "airport lounge"
+        }
+
+
+def Quick_facts():
+    q_facts = {
+        "1. What year was Hampshire College opened?" : 1970,
+        "2. Is Hampshire College a private or public school?" : "private",
+        '3. What is the "unofficial" school mascot?' : "frog" or "bullfrog",
+        "4. Is Hampshire a small, medium, or large school?" : "small",
+        "5. What is the unique speed limit of Hampshire College campus?" : 17
+        }
+    
+##    q1 = input("1. What year was Hampshire College opened? ")
+##    q1 = q1.lower()
+##    while q1 != "1970":
+##        #Request another input and decrease the score equal to half the score_modifier * 1
+##        q1 = input("Incorrect, please try again: ")
+##        q1 = q1.lower()
+##        score -= 1 * score_modifier / 2
+##        #When the inputted answer is the correct answer
+##    else:
+##        #Add to the score equal to the score_modifier
+##        score += 1 * score_modifier
+##        #Print correct and move onto the next question
+##        print("Correct!")
+##        print()
+
+
+
+def Presidents():
+    ...
+
+
