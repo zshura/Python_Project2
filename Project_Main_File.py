bdea9bb26a5d930cb8b07a7fc4d90ee4445436d5
DESKTOP-UJIMONJ\zofia <zhs17@hampshire.edu>
2018-05-04 13:06:25 -0400

    Finished base coding

--- Project_Main_File.py
+++ Project_Main_File.py
@@ -1,11 +1,5 @@
 #ZShura_Project2
 
-# from Project_Questions import Anagrams, Quick_facts, Presidents
-
-# Checklist:
-# - Create all the doc strings
-# - Finish Presidents
-
 def Startup():
     """The Start_up function begins the program by asking what round of questions you would like to take."""
 
@@ -14,7 +8,9 @@ def Startup():
     level = input("What set of questions would you like to play? ")
     level = level.lower()
     print("Thank you for choosing", level, "!")
-    
+    return level, Level_choice(level)
+
+def Level_choice(level):
     if level == "anagrams":
         from Project_Questions import Anagrams
         d = Anagrams()
@@ -26,10 +22,15 @@ def Startup():
     if level == "presidents":
         from Project_Questions import Presidents
         d = Presidents()
+        print("Please be aware Presidents are listed by first name and last name, but not middle initial.")
         return d, Game(d)
+    if level == "bonus":
+        from Project_Questions import Bonus
+        d = Bonus()
+        return d, Bonus(d)
     else:
         print("Incorrect input. Please try again.")
-           
+       
 
 def Game(d):
     score = 0
@@ -37,29 +38,34 @@ def Game(d):
         answer = input(key)
         answer = answer.lower()
         while answer != value:
-            answer = input("whoopsie")
+            answer = input("Incorrect, please try again: ")
             score -= 1
         else:
              print("Correct!")
              score += 1
-    print(score)
-        
-    
-    
+    return score, End_Game(score)
 
 
-##def End_Game(score):
-##    """The End_Game function will close out the program once it has been run through after
-##    providing the player with their final score. It also offers a chance to replay the game."""
-##    
-##    print("Good job! You have completed the trivia.\nYour final score is ", score)
-##    print()
-##    replay = input("Would you like to play again? ")
-##    print()
-##    if replay == "yes":
-##        return startup()
-##    if replay == "no":
-##        print("Thanks for playing!")
+def End_Game(score):
+    """The End_Game function will close out the program once it has been run through after
+    providing the player with their final score. It also offers a chance to replay the game."""
+    
+    print("Good job! You have completed the trivia.\nYour final score is ", score)
+    print()
+    level = input("Would you like to play the bonus round?: ")
+    level = level.lower
+    if level == "yes":
+        level = "bonus" 
+        return level, Level_choice(level)
+    if level == "no":
+        pass
+    replay = input("Would you like to play a different level?: ")
+    replay = replay.lower()
+    print()
+    if replay == "yes":
+        return startup()
+    if replay == "no":
+        print("Thanks for playing!")
 
 
 d = Startup()
--- Project_Questions.py
+++ Project_Questions.py
@@ -1,7 +1,7 @@
 def Anagrams():
     anagrams = {
         "1. What does ASH stand for? " : "adele simmons hall",
-        "2. What does FPL stand for? " : "...",
+        "2. What does FPH stand for? " : "franklin patterson hall",
         "3. What does EDH stand for? " : "emily dickinson hall",
         "4. What does HJL stand for? " : "harold johnson library",
         "5. What does APL stand for? " : "airport lounge"
@@ -22,9 +22,20 @@ def Quick_facts():
 def Presidents():
     pres = {
         "1. Who is the current (Spring 2018) President? " : "jonathan lash",
-        "2. Who is the future President? " : ""
-
+        "2. Who is the future President? " : "miriam nelson",
+        "3. Who is the only (currently) deceased President? " : "franklin patterson",
+        "4. Who is the second President to have a campus building named after them? " : "adele simmons",
+        "5. Who is the only Interim President? " : "marlene gerber fried"
         }
     return pres
+
+def Bonus():
+    bonus = {
+        "1. Is the Drome in ASH, Lemmelson, or FPH? " : "ash",
+        "2. The library has how many levels? " : "5",
+        "3. Is the pool big enough to have swim team? " : "no",
+        "4. How many groups of mods are there? " : "3", 
+        "5. What is a popular lunch location for first years?" : "bridge"
+        }
 
 
